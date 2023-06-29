b8c01a7014c1626542c9ab9a73c07dd993911faf
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-06-29 22:18:43 +0100

    added some content

--- src/routes/user.mjs
+++ src/routes/user.mjs
@@ -1,22 +1,22 @@
 // USER ROUTES
 import { Router } from "express";
 import UserController from "../controller/user.mjs";
-import schemaValidationHelper from "../validation/schemaValidationHelper.mjs";
-import { updateUser } from "../validation/schema/user.mjs";
+import SchemaValidation from "../validation/schemaValidationHelper.mjs";
+import { signUp, updateUser } from "../validation/schema/user.mjs";
 import Authorization from "../middleware/authorization.mjs";
 
 const router = Router();
 
-router.get("/all", UserController.get_all_user);
-router.get("/:id", UserController.get_a_user);
+router.get("/all", Authorization, UserController.get_all_user);
+router.get("/:id", Authorization, UserController.get_a_user);
 
-router.post("/signUp", UserController.signUp);
+router.post("/signUp", SchemaValidation.validateSchema(signUp), UserController.signUp);
 router.post("/signIn", UserController.signIn);
 
-router.put("/update/:id", UserController.updateUser);
-router.put("/:id/follow", UserController.follow_a_user);
-router.put("/:id/unfollow", UserController.unfollow_a_user);
+router.put("/update/:id", Authorization, SchemaValidation.validateSchema(updateUser), UserController.updateUser);
+router.put("/:id/follow", Authorization, UserController.follow_a_user);
+router.put("/:id/unfollow", Authorization, UserController.unfollow_a_user);
 
 router.delete("/delete/:id", Authorization, UserController.deleteUser);
 
-export default router;
+export default router
