f7cea7c3b7f6a5a871ba7dcf2a70bc9417a5e62f
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-07-14 01:34:11 +0100

    Updated and also added some content

--- src/util/helperFunction.mjs
+++ src/util/helperFunction.mjs
@@ -3,6 +3,8 @@ import ResponseHelper from "./responseHelper.mjs";
 import bcrypt from "bcrypt";
 import crypto from "crypto"
 import mongoose from 'mongoose'
+import nodemailer from 'nodemailer';
+import { MAIL, MAIL_P } from '../config/keys.mjs'
 
 class HelperFunction {
   /**
@@ -42,28 +44,53 @@ class HelperFunction {
    * @returns - RETURNS A RESET TOKEN
    * @memberof HelperFunction
    */
-  static async generatePasswordResetToken() {
-    const resetToken = crypto.randomBytes(30).toString("hex");
-    this.passwordResetToken = crypto
-      .createHash("SHA256")
+  static async generatePasswordResetToken(user) {
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    user.passwordResetToken = crypto
+      .createHash("sha256")
       .update(resetToken)
       .digest("hex");
-    this.passwordResetTokenExpiresAt = Date.now() + 10 * 60 * 1000;
+    user.passwordResetTokenExpiresAt = Date.now() + 10 * 60 * 1000; // Set expiration to 10 minutes
+    await user.save(); // Save the updated user object
     return resetToken;
   }
 
-  /**
-   * @description - THIS IS USED TO GENERATE A TOKEN
-   * @returns - RETURNS A TOKEN
-   */
-  static async generateResetToken() {
-    const resetToken = crypto.randomBytes(32).digest('hex');
-    this.passwordResetToken = crypto.createHash('SHA256').update(resetToken).digest('hex');
-    this.passwordResetTokenExpiresAt = Date.now() + 10 * 30 * 1000;
-    return resetToken
+  static async sendMail(data, req, res) {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: MAIL,
+        pass: MAIL_P,
+      }
+    });
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+      // send mail with defined transport object
+      const info = await transporter.sendMail({
+        from: process.env.MAIL, // sender address
+        to: data.to, // list of receivers
+        subject: data.subject, // Subject line
+        text: data.text, // plain text body
+        html: data.htm, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+      //
+      // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+      //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+      //       <https://github.com/forwardemail/preview-email>
+      //
+    }
+
+    main().catch(console.error);
+
   }
-
-
 }
 
 export default HelperFunction;
