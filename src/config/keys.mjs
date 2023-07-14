fa151863f06f069e249b19438a8b76abfc95a863
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-07-14 01:34:41 +0100

    Added some content

--- src/config/keys.mjs
+++ src/config/keys.mjs
@@ -4,4 +4,6 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const DB = process.env.DATABASE;
 export const SECRET = process.env.SECRET;
+export const MAIL = process.env.MAIL;
+export const MAIL_P = process.env.MAIL_P
 
