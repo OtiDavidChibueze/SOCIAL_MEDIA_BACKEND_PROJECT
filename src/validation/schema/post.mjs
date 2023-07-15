26b9538c0ccc32e11878c7dc05ebe4579824d870
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-07-15 17:45:42 +0100

    Update post schemaValidation

--- src/validation/schema/post.mjs
+++ src/validation/schema/post.mjs
@@ -1,24 +1,26 @@
-// POST SCHEMA VALIDATION
+//* POST SCHEMA VALIDATION
 import joi from "joi";
 
-export const postValidation = joi.object(
-  {
-    userId: joi.string().regex(/^[a-fA-F0-9]{24}$/),
-    desc: joi.string().max(50).required(),
-    img: joi.string(),
-    like: joi.array(),
-    dislike: joi.array(),
-    liked: joi.array(),
-    disliked: joi.array(),
-    // comments: joi.array().options(
-    //   joi.object({
-    //     commentedBy: joi
-    //       .string()
-    //       .regex(/^[a-fA-F0-9]{24}$/)
-    //       .required(),
-    //     comment: joi.string().max(50).required(),
-    //   })
-    // ),
-  },
-  { stripUnknown: true }
-);
+export const postValidation = joi.object({
+  userId: joi.string().regex(/^[a-fA-F0-9]{24}$/),
+  desc: joi.string().required().max(500),
+  img: joi.string().optional(),
+  video: joi.string().optional(),
+  like: joi.number().default(0),
+  dislike: joi.number().default(0),
+  liked: joi.array().default([]),
+  disliked: joi.array().default([]),
+  comments: joi.array().items(
+    joi.object({
+      commentedBy: joi.string().required().regex(/^[a-fA-F0-9]{24}$/),
+      comment: joi.string().required().max(500),
+    })
+  ),
+});
+
+export const updatePost = joi.object({
+  desc: joi.string().required().max(500),
+  img: joi.string().optional(),
+  video: joi.string().optional(),
+
+});
